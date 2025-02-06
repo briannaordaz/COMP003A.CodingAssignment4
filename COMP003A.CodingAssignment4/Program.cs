6e5c03aac1b9e75ea392567d22224fd12d5b41de
briannaordaz <briannaordaz5@gmail.com>
2025-02-05 18:24:08 -0800

    added author, course, faculty, purpose

---
+++ COMP003A.CodingAssignment4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Author: Brianna Ordaz
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+//Purpose: Inventory management application with a minimum of 10 elements in the collection.
+namespace COMP003A.CodingAssignment4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
